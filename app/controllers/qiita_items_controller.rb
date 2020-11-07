da1f3d33c7c7347343e9272a9840a86b685742e7
nishina555 <n.toshi.1.014@gmail.com>
2020-11-07 20:34:19 +0900

    シンプルなパターン

--- app/controllers/qiita_items_controller.rb
+++ app/controllers/qiita_items_controller.rb
@@ -1,8 +1,18 @@
 class QiitaItemsController < ApplicationController
   # GET /qiita_items
   def index
-    @qiita_items = []
-
-    render json: @qiita_items
+    # require 'net/http'
+    uri = URI.parse('https://qiita.com')
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    req = Net::HTTP::Get.new(
+      '/api/v2/items',
+      'Authorization' => "Bearer #{Rails.application.credentials.qiita[:token]}"
+    )
+    res = http.request(req)
+    @qiita_items = res.body
+    response_json = JSON.parse(@qiita_items)
+    render json: response_json
+    # render json: response_json.map {|item| item.slice('title') }
   end
 end
