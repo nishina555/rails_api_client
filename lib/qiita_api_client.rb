e09e5c80166c64c2a661a794ef0ca9b932d4c990
nishina555 <n.toshi.1.014@gmail.com>
2020-11-08 14:39:19 +0900

    例外処理を追加

--- lib/qiita_api_client.rb
+++ lib/qiita_api_client.rb
@@ -1,17 +1,29 @@
 class QiitaApiClient
+  # class HTTPError < StandardError
+  #   def initialize(response)
+  #     super "code=#{response.code} body=#{response.body}"
+  #   end
+  # end
+
   def initialize
-    @token = Rails.application.credentials.qiita[:token]
+    @token = 'hoge'
+    # @token = Rails.application.credentials.qiita[:token]
   end
 
   def get_items
-    req = Net::HTTP::Get.new(
+    request = Net::HTTP::Get.new(
       '/api/v2/items',
       'Authorization' => "Bearer #{@token}"
     )
-    http_client.request(req)
-    res = http_client.request(req)
-    @qiita_items = res.body
-    JSON.parse(@qiita_items)
+    http_client.request(request)
+    response = http_client.request(request)
+    case response
+    when Net::HTTPSuccess
+      JSON.parse(response.body)
+    else
+      raise "code= #{response.code}, body = #{response.body}"
+      # raise QiitaApiClient::HTTPError.new(response)
+    end
   end
 
   class << self
@@ -37,8 +49,11 @@ class QiitaApiClient
   end
 
   private
+
+  QIITA_HOST = 'https://qiita.com'
+
   def http_client
-    uri = URI.parse('https://qiita.com')
+    uri = URI.parse(QIITA_HOST)
     http_client = Net::HTTP.new(uri.host, uri.port)
     http_client.use_ssl = true
     http_client
