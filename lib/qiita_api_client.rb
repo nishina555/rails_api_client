74f841365e17edde2da046488d2a6163c93bcb41
nishina555 <n.toshi.1.014@gmail.com>
2020-11-08 13:26:14 +0900

    APIクライアントにhttp_clientメソッドを作成する

--- lib/qiita_api_client.rb
+++ lib/qiita_api_client.rb
@@ -1,18 +1,40 @@
 class QiitaApiClient
-  class << self
-    def get_items
-      # require 'net/http'
-      uri = URI.parse('https://qiita.com')
-      http = Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-      req = Net::HTTP::Get.new(
-        '/api/v2/items',
-        'Authorization' => "Bearer #{Rails.application.credentials.qiita[:token]}"
-      )
-      http.request(req)
-      res = http.request(req)
-      @qiita_items = res.body
-      JSON.parse(@qiita_items)
-    end
+  def initialize
+    @token = Rails.application.credentials.qiita[:token]
+  end
+  def get_items
+    req = Net::HTTP::Get.new(
+      '/api/v2/items',
+      'Authorization' => "Bearer #{@token}"
+    )
+    http_client.request(req)
+    res = http_client.request(req)
+    @qiita_items = res.body
+    JSON.parse(@qiita_items)
+  end
+
+  # class << self
+  #   def get_items
+  #     # require 'net/http'
+  #     uri = URI.parse('https://qiita.com')
+  #     http = Net::HTTP.new(uri.host, uri.port)
+  #     http.use_ssl = true
+  #     req = Net::HTTP::Get.new(
+  #       '/api/v2/items',
+  #       'Authorization' => "Bearer #{Rails.application.credentials.qiita[:token]}"
+  #     )
+  #     http.request(req)
+  #     res = http.request(req)
+  #     @qiita_items = res.body
+  #     JSON.parse(@qiita_items)
+  #   end
+  # end
+
+  private
+  def http_client
+    uri = URI.parse('https://qiita.com')
+    http_client = Net::HTTP.new(uri.host, uri.port)
+    http_client.use_ssl = true
+    http_client
   end
 end
