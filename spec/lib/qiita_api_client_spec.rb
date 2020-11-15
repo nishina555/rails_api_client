f7df9da42d5cf244307f654ee328bbf3ed599a87
nishina555 <n.toshi.1.014@gmail.com>
2020-11-15 18:20:07 +0900

    共通部分をsubjectにする

--- spec/lib/qiita_api_client_spec.rb
+++ spec/lib/qiita_api_client_spec.rb
@@ -7,6 +7,7 @@ describe 'QiitaApiClient' do
   end
 
   describe '.get_items' do
+    subject { QiitaApiClient.get_items }
     context '成功' do
       let(:response_body) { [{ "title": "test" }].to_json }
       before do
@@ -21,8 +22,8 @@ describe 'QiitaApiClient' do
           )
       end
       it 'データが取得できること' do
-        response_body = QiitaApiClient.get_items
-        expect(response_body).to eq(response_body)
+        # expect(QiitaApiClient.get_items).to eq(JSON.parse(response_body))
+        expect(subject).to eq(JSON.parse(response_body))
       end
     end
     context '失敗' do
@@ -34,12 +35,12 @@ describe 'QiitaApiClient' do
           ).
           to_return(
             body: response_body,
-            status: 400,
+            status: 500,
             headers: { 'Content-Type' =>  'application/json' }
           )
       end
       it '例外が発生すること' do
-        expect{ QiitaApiClient.get_items }.to raise_error(QiitaApiClient::HTTPError, "status=400 body=#{JSON.parse(response_body)}" )
+        expect(subject).to raise_error(QiitaApiClient::HTTPError, "status=500 body=#{JSON.parse(response_body)}")
       end
     end
   end
